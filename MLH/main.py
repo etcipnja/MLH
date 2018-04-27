ff5c5dd94154f55e15abe00a97dc08deaa2f9445
etcipnja <eugene@wydevoice.com>
2018-04-26 23:52:19 -0400

    preparation to test on the bot

--- MLH/main.py
+++ MLH/main.py
@@ -28,12 +28,13 @@ class Farmware():
     # ------------------------------------------------------------------------------------------------------------------
     def log(self, message, message_type='info'):
 
-        # try:
-        #    log_message = '[{}] {}'.format(APP_NAME, message)
-        #    node = {'kind': 'send_message', 'args': {'message': log_message, 'message_type': message_type}}
-        #    ret = requests.post(os.environ['FARMWARE_URL']+'api/v1/celery_script', data=json.dumps(node), headers=self.headers)
-        #    message = log_message
-        # except: pass
+        try:
+            log_message = '[{}] {}'.format(APP_NAME, message)
+            node = {'kind': 'send_message', 'args': {'message': log_message, 'message_type': message_type}}
+            response = requests.post(os.environ['FARMWARE_URL']+'api/v1/celery_script', data=json.dumps(node), headers=self.headers)
+            self.handle_error(response)
+            message = log_message
+        except: pass
 
         print(message)
 
@@ -86,7 +87,14 @@ class MLH(Farmware):
         prefix = APP_NAME.lower().replace('-', '_')
         self.params = {}
 
-        filter = os.environ.get(prefix + "_filter_meta", "[('plant_stage','planted')]")
+        self.params['pointname'] = os.environ.get(prefix + "_pointname", '*')
+        self.params['sequence'] = {'init': {'name': os.environ.get(prefix + '_init', 'None'), 'id': -1},
+                                   'before': {'name': os.environ.get(prefix + '_before', 'None'), 'id': -1},
+                                   'after': {'name': os.environ.get(prefix + '_after', 'None'), 'id': -1},
+                                   'end': {'name': os.environ.get(prefix + '_end', 'None'), 'id': -1}}
+        self.params['default_z'] = int(os.environ.get(prefix + "_default_z", -300))
+        self.params['action'] = os.environ.get(prefix + "_action", 'test')
+        filter = os.environ.get(prefix + "_filter_meta", "None")
         save = os.environ.get(prefix + "_save_meta", "None")
 
         try:
@@ -100,14 +108,7 @@ class MLH(Farmware):
         except:
             raise ValueError("Invalid meta {} or {}".format(filter, save))
 
-        self.params['pointname'] = os.environ.get(prefix + "_pointname", 'Beets')
-        self.params['sequence'] = {'init': {'name': os.environ.get(prefix + '_init', 'Pickup seeder'), 'id': -1},
-                                   'before': {'name': os.environ.get(prefix + '_before', 'Grab a seed'), 'id': -1},
-                                   'after': {'name': os.environ.get(prefix + '_after', 'Plant a seed'), 'id': -1},
-                                   'end': {'name': os.environ.get(prefix + '_end', 'Return seeder'), 'id': -1}}
 
-        self.params['default_z'] = int(os.environ.get(prefix + "_default_z", -300))
-        self.params['action'] = os.environ.get(prefix + "_action", 'test')
 
         self.log(str(self.params))
 
